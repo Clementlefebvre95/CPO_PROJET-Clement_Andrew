71eb76973653d49442b79496afa292567cd6bc74
ferre <ferre@10.3.62.14>
2024-11-29 15:14:22 +0100

    Création d'un nouveau projet versionné

--- projet_demineurCA/src/projet_demineurca/Projet_demineurCA.java
+++ projet_demineurCA/src/projet_demineurca/Projet_demineurCA.java
@@ -15,6 +15,14 @@ public class Projet_demineurCA {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Création d'une instance de Partie
+        Partie partie = new Partie();
+
+        // Initialisation de la partie avec une grille 5x5 et 5 bombes (modifiable)
+        partie.initialiserPartie(5, 5, 5);
+
+        // Démarrage de la partie
+        partie.demarrerPartie();
+    }
     }
     
-}
