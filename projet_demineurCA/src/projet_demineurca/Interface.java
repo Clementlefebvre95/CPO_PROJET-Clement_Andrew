590ec68c1e4821e92e0f4f5f516c12f9228398be
ferre <ferre@10.3.62.14>
2024-12-13 10:24:31 +0100

    Création d'un nouveau projet versionné

--- projet_demineurCA/src/projet_demineurca/Interface.java
+++ projet_demineurCA/src/projet_demineurca/Interface.java
@@ -254,10 +254,8 @@ public class Interface extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Interface().setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            new Interface().setVisible(true);
         });
     }
 
--- projet_demineurCA/src/projet_demineurca/Partie.java
+++ projet_demineurCA/src/projet_demineurca/Partie.java
@@ -24,14 +24,15 @@ public class Partie {
     public void demarrerPartie(Scanner scanner) {
         while (true) {
             System.out.println(grille.toString());
-            System.out.println("Entrez les coordonnées (ligne et colonne) pour révéler une cellule :");
+            System.out.println("Entrez les coordonnees pour reveler une cellule :");
+            System.out.println("Envoyer la ligne puis ensuite la colonne");
 
             int ligne = scanner.nextInt();
             int colonne = scanner.nextInt();
 
             // Vérification des coordonnées
             if (ligne < 0 || ligne >= grille.getNbLignes() || colonne < 0 || colonne >= grille.getNbColonnes()) {
-                System.out.println("Coordonnées invalides, essayez encore.");
+                System.out.println("Coordonnees invalides, essayez encore.");
                 continue;
             }
 
